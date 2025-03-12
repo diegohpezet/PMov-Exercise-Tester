6533a239b3e1826253867a7563a19b2a2de00efa
diegohpezet <diegohpezet@gmail.com>
2025-03-12 18:46:35 -0300

    fix dependencies (again) & port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import express from 'express';
 import router from './routes/index.js';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 35678;
 
 app.use(router);
 
